1b03d277e181983340bc6a9dce13768ed5f4be44
Alessio Chessa <ale.chessa28@gmail.com>
2025-12-01 20:59:22 +0100

    fix: removed main from aleche28 solutions folder

--- 2025/aleche28/day01/main.py
+++ 2025/aleche28/day01/main.py
@@ -3,48 +3,53 @@ def read_file_lines(filename):
         return [x.strip() for x in f]
 
 
-lines = read_file_lines("input.txt")
+def main():
+    lines = read_file_lines("input.txt")
 
-sum = 50
-cntSumZero = 0
-for line in lines:
-    if line[0] == 'L':
-        # print(str(sum) + " - " + l[1:], end="")
-        sum -= int(line[1:])
-    else:
-        # print(str(sum) + " + " + l[1:], end="")
-        sum += int(line[1:])
-    sum %= 100
-    # print(" = " + str(sum))
-    if sum == 0:
-        cntSumZero += 1
-
-print("Solution part 1: " + str(cntSumZero))
-
-sum = 50
-cntSumZero = 0
-for line in lines:
-    n = int(line[1:])
-    clicks = int(n / 100)
-    mod = n % 100
-    prev_sum = sum
-
-    if line[0] == 'R':
-        # print(str(sum) + " + " + line[1:], end="")
-        sum += mod
-    else:
-        # print(str(sum) + " - " + line[1:], end="")
-        sum -= mod
-
-    if mod > 0:
-        if sum == 0:
-            clicks += 1
+    sum = 50
+    cntSumZero = 0
+    for line in lines:
+        if line[0] == 'L':
+            # print(str(sum) + " - " + l[1:], end="")
+            sum -= int(line[1:])
         else:
-            if sum != (sum % 100):
-                sum %= 100
-                if prev_sum != 0:
-                    clicks += 1
-    cntSumZero += clicks
-    # print(" = " + str(sum) + " | " + str(clicks))
+            # print(str(sum) + " + " + l[1:], end="")
+            sum += int(line[1:])
+        sum %= 100
+        # print(" = " + str(sum))
+        if sum == 0:
+            cntSumZero += 1
 
-print("Solution part 2: " + str(cntSumZero))
+    print("Solution part 1: " + str(cntSumZero))
+
+    sum = 50
+    cntSumZero = 0
+    for line in lines:
+        n = int(line[1:])
+        clicks = int(n / 100)
+        mod = n % 100
+        prev_sum = sum
+
+        if line[0] == 'R':
+            # print(str(sum) + " + " + line[1:], end="")
+            sum += mod
+        else:
+            # print(str(sum) + " - " + line[1:], end="")
+            sum -= mod
+
+        if mod > 0:
+            if sum == 0:
+                clicks += 1
+            else:
+                if sum != (sum % 100):
+                    sum %= 100
+                    if prev_sum != 0:
+                        clicks += 1
+        cntSumZero += clicks
+        # print(" = " + str(sum) + " | " + str(clicks))
+
+    print("Solution part 2: " + str(cntSumZero))
+
+
+if __name__ == "__main__":
+    main()
--- 2025/aleche28/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from aleche28!")
-
-
-if __name__ == "__main__":
-    main()
