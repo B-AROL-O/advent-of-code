27333cacb045832cf979acf2a3ed78b0bb821c49
Alessio Chessa <ale.chessa28@gmail.com>
2025-12-02 23:39:39 +0100

    feat: aleche28 day02 part 2 solution

--- 2025/aleche28/day02/main.py
+++ 2025/aleche28/day02/main.py
@@ -9,15 +9,34 @@ def main():
     for interval in intervals:
         a = int(interval.split('-')[0])
         b = int(interval.split('-')[1])
-        print(a, b)
+        # print(a, b)
         for i in range(a, b+1):
             if len(str(i)) % 2 == 0:
                 numstr = str(i)
                 h = int(len(numstr) / 2)
                 if numstr[:h] == numstr[h:]:
-                    print("invalid: " + numstr)
+                    # print("invalid: " + numstr)
                     sum += i
-    print(sum)
+    print("Solution part 1: " + str(sum))
+
+    sum = 0
+    for interval in intervals:
+        a = int(interval.split('-')[0])
+        b = int(interval.split('-')[1])
+        # print(a, b)
+        for i in range(a, b+1):
+            l = len(str(i))
+            numstr = str(i)
+            for d in range(1, int(l/2)+1):
+                if l % d > 0:
+                    continue
+                t = int(l/d)
+                if numstr[:d] * t == numstr:
+                    # print("invalid: " + numstr)
+                    sum += i
+                    break
+
+    print("Solution part 2: " + str(sum))
 
 
 if __name__ == "__main__":
