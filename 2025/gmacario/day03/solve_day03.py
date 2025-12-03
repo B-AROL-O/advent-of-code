92a1c0b7325afa9214f5e1187e7cd36be30500c8
Gianpaolo Macario <gmacario@gmail.com>
2025-12-03 18:03:05 +0000

    feat(2025/gmacario): day03 part2 solved with help from gpt-oss:120b

--- 2025/gmacario/day03/solve_day03.py
+++ 2025/gmacario/day03/solve_day03.py
@@ -8,10 +8,18 @@ CHALLENGE_DAY=3
 
 CHALLENGE_URL=f"https://adventofcode.com/2025/day/{CHALLENGE_DAY}"
 INPUT_FILE=f"day{CHALLENGE_DAY:02}/sample_day{CHALLENGE_DAY:02}.txt"
-# INPUT_FILE=f"day{CHALLENGE_DAY:02}/input_day{CHALLENGE_DAY:02}.txt"
+INPUT_FILE=f"day{CHALLENGE_DAY:02}/input_day{CHALLENGE_DAY:02}.txt"
 
 print(f"INFO:  Advent of Code 2025 - Day {CHALLENGE_DAY}")
 print(f"INFO:  URL: {CHALLENGE_URL}")
+
+# ic()
+
+# Read the puzzle input into a list of strings, one per line
+with open(INPUT_FILE, 'r') as file:
+    input_lines = [line.rstrip() for line in file]
+
+# ic(input_lines)
 
 def find_largest_joltage(bank: str) -> int:
     result = 0
@@ -27,35 +35,71 @@ def find_largest_joltage(bank: str) -> int:
     # ic(f"find_largest_joltage({bank}) return {result}")
     return result
 
-# ic()
 
-# Read the puzzle input into a list of strings, one per line
-with open(INPUT_FILE, 'r') as file:
-    input_lines = [line.rstrip() for line in file]
+# Credits: gpt-oss:120b running on openwebui.gmacario.it
+#
+# Prompt:
+#   Write a program in Python to solve Part Two of the following challenge:
+#   (paste the contents of https://adventofcode.com/2025/day/3)
+# 
+def max_k_subseq(s: str, k: int = 12) -> int:
+    """
+    Return the integer represented by the lexicographically largest
+    subsequence of length k of the digit string s.
+    """
+    stack = []                         # will contain characters, not ints
+    n = len(s)
 
-# ic(input_lines)
+    for i, c in enumerate(s):
+        # While we can improve the prefix by discarding a smaller digit,
+        # and we still have enough characters left to reach length k,
+        # pop the stack.
+        while (stack and stack[-1] < c and
+               (len(stack) - 1) + (n - i) >= k):
+            stack.pop()
+
+        # If we still need more characters, push the current one.
+        if len(stack) < k:
+            stack.append(c)
+        # else: we already have k characters – ignore the rest.
+
+    # At this point len(stack) == k
+    return int(''.join(stack))
 
 
 def solve_part1():
-    print(f"DEBUG: solve_part1 Begin: {time.ctime()}")
-
-    # print("DEBUG: TODO solve_part1()")
+    tm_start = time.time()
     result_part1 = 0
+
+    # ic("DEBUG: TODO solve_part1()")
     for bank in input_lines:
         result_part1 += find_largest_joltage(bank)
 
-    print(f"DEBUG: solve_part1 End:   {time.ctime()}")
+    tm_end = time.time()
+    print(f"DEBUG: solve_part1 Begin: {time.ctime(tm_start)}")
+    print(f"DEBUG: solve_part1 End:   {time.ctime(tm_end)}")
+    print(f"DEBUG: solve_part1 Delta: {tm_end-tm_start}")
     print(f"INFO:  Day{CHALLENGE_DAY:02} solve_part1 result: {result_part1}")
     return result_part1
 
 
 def solve_part2():
-    print(f"DEBUG: solve_part1 Begin: {time.ctime()}")
+    tm_start = time.time()
+    result_part2 = 0
 
-    print("DEBUG: TODO solve_part2()")
-    result_part2 = 0   # TODO
+    # ic("DEBUG: TODO solve_part2()")
+    line_count = 0
+    for line in input_lines:
+        line_count += 1
+        # ic(line_count, len(line))
+        res = max_k_subseq(line)
+        # ic(f"max_k_subseq({line}) is {res}")
+        result_part2 += res
 
-    print(f"DEBUG: solve_part1 End:   {time.ctime()}")
+    tm_end = time.time()
+    print(f"DEBUG: solve_part2 Begin: {time.ctime(tm_start)}")
+    print(f"DEBUG: solve_part2 End:   {time.ctime(tm_end)}")
+    print(f"DEBUG: solve_part2 Delta: {tm_end-tm_start}")
     print(f"INFO:  Day{CHALLENGE_DAY:02} solve_part2 result: {result_part2}")
     return result_part2
 
