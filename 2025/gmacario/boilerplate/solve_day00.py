4441fb8857ced4488295d334e487f5738280f1c7
Gianpaolo Macario <gmacario@gmail.com>
2025-12-03 15:51:36 +0000

    feat(2025/gmacario): improve boilerplate

--- 2025/gmacario/boilerplate/solve_day00.py
+++ 2025/gmacario/boilerplate/solve_day00.py
@@ -10,40 +10,39 @@ CHALLENGE_URL=f"https://adventofcode.com/2025/day/{CHALLENGE_DAY}"
 INPUT_FILE=f"day{CHALLENGE_DAY:02}/sample_day{CHALLENGE_DAY:02}.txt"
 # INPUT_FILE=f"day{CHALLENGE_DAY:02}/input_day{CHALLENGE_DAY:02}.txt"
 
-print(f"Advent of Code 2025 - Day {CHALLENGE_DAY}")
-print(f"URL: {CHALLENGE_URL}")
+print(f"INFO:  Advent of Code 2025 - Day {CHALLENGE_DAY}")
+print(f"INFO:  URL: {CHALLENGE_URL}")
 
 
 # ic()
 
 # Read the puzzle input into a list of strings, one per line
-with open(INPUT_FILE, 'r') as file:
-    input_lines = [line.rstrip() for line in file]
+# with open(INPUT_FILE, 'r') as file:
+#     input_lines = [line.rstrip() for line in file]
 
-ic(input_lines)
+# ic(input_lines)
 
 
 def solve_part1():
-    ic(time.ctime())
+    print(f"DEBUG: solve_part1 Begin: {time.ctime()}")
 
-    ic("TODO solve_part1()")
+    print("DEBUG: TODO solve_part1()")
     result_part1 = 0   # TODO
 
-    print(f"Day{CHALLENGE_DAY:02} Part 1 result: {result_part1}")
-    
-    ic(time.ctime())
+    print(f"DEBUG: solve_part1 End:   {time.ctime()}")
+    print(f"INFO:  Day{CHALLENGE_DAY:02} solve_part1 result: {result_part1}")
+    return result_part1
 
 
 def solve_part2():
-    ic(time.ctime())
+    print(f"DEBUG: solve_part1 Begin: {time.ctime()}")
 
-    ic("TODO solve_part2()")
+    print("DEBUG: TODO solve_part2()")
     result_part2 = 0   # TODO
 
-    print(f"Day{CHALLENGE_DAY:02} Part 2 result: {result_part2}")
-
-    ic(time.ctime())
-    pass
+    print(f"DEBUG: solve_part1 End:   {time.ctime()}")
+    print(f"INFO:  Day{CHALLENGE_DAY:02} solve_part2 result: {result_part2}")
+    return result_part2
 
 
 if __name__ == "__main__":
