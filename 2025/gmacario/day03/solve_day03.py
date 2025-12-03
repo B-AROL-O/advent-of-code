ba527a2d1811ae652e757605ca7d8f31e73a7169
Gianpaolo Macario <gmacario@gmail.com>
2025-12-03 15:55:32 +0000

    feat(2025/gmacario): align solve_day03 with boilerplate

--- 2025/gmacario/day03/solve_day03.py
+++ 2025/gmacario/day03/solve_day03.py
@@ -10,9 +10,8 @@ CHALLENGE_URL=f"https://adventofcode.com/2025/day/{CHALLENGE_DAY}"
 INPUT_FILE=f"day{CHALLENGE_DAY:02}/sample_day{CHALLENGE_DAY:02}.txt"
 # INPUT_FILE=f"day{CHALLENGE_DAY:02}/input_day{CHALLENGE_DAY:02}.txt"
 
-print(f"Advent of Code 2025 - Day {CHALLENGE_DAY}")
-print(f"URL: {CHALLENGE_URL}")
-
+print(f"INFO:  Advent of Code 2025 - Day {CHALLENGE_DAY}")
+print(f"INFO:  URL: {CHALLENGE_URL}")
 
 def find_largest_joltage(bank: str) -> int:
     result = 0
@@ -34,31 +33,31 @@ def find_largest_joltage(bank: str) -> int:
 with open(INPUT_FILE, 'r') as file:
     input_lines = [line.rstrip() for line in file]
 
-ic(input_lines)
+# ic(input_lines)
 
 
 def solve_part1():
-    ic(time.ctime())
+    print(f"DEBUG: solve_part1 Begin: {time.ctime()}")
 
-    result_part1 = 0;
+    # print("DEBUG: TODO solve_part1()")
+    result_part1 = 0
     for bank in input_lines:
         result_part1 += find_largest_joltage(bank)
 
-    print(f"Day{CHALLENGE_DAY:02} Part 1 result: {result_part1}")
-    
-    ic(time.ctime())
+    print(f"DEBUG: solve_part1 End:   {time.ctime()}")
+    print(f"INFO:  Day{CHALLENGE_DAY:02} solve_part1 result: {result_part1}")
+    return result_part1
 
 
 def solve_part2():
-    ic(time.ctime())
+    print(f"DEBUG: solve_part1 Begin: {time.ctime()}")
 
-    ic("TODO solve_part2()")
-    result_part2 = 0;   # TODO
+    print("DEBUG: TODO solve_part2()")
+    result_part2 = 0   # TODO
 
-    print(f"Day{CHALLENGE_DAY:02} Part 2 result: {result_part2}")
-
-    ic(time.ctime())
-    pass
+    print(f"DEBUG: solve_part1 End:   {time.ctime()}")
+    print(f"INFO:  Day{CHALLENGE_DAY:02} solve_part2 result: {result_part2}")
+    return result_part2
 
 
 if __name__ == "__main__":
