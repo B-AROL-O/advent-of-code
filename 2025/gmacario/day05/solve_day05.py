55d494ded0f228a2e833428e04c92c3a8e36b447
Gianpaolo Macario <gmacario@gmail.com>
2025-12-05 17:33:43 +0000

    feat(2025/gmacario): solve day05 part1

---
+++ 2025/gmacario/day05/solve_day05.py
@@ -0,0 +1,87 @@
+import time
+
+from icecream import ic
+
+CHALLENGE_DAY=5
+
+CHALLENGE_URL=f"https://adventofcode.com/2025/day/{CHALLENGE_DAY}"
+INPUT_FILE=f"day{CHALLENGE_DAY:02}/sample_day{CHALLENGE_DAY:02}.txt"
+INPUT_FILE=f"day{CHALLENGE_DAY:02}/input_day{CHALLENGE_DAY:02}.txt"
+
+print(f"INFO:  Advent of Code 2025 - Day {CHALLENGE_DAY}")
+print(f"INFO:  URL: {CHALLENGE_URL}")
+print(f"INFO:  INPUT_FILE: {INPUT_FILE}")
+
+# ic()
+
+# Read the puzzle input into a list of strings, one per line
+with open(INPUT_FILE, 'r') as file:
+    input_lines = [line.rstrip() for line in file]
+
+# ic(input_lines)
+
+fresh_ingredients = []
+available_ingredients = []
+find_fresh_ingredients = True
+
+for l in input_lines:
+    if l == '':
+        find_fresh_ingredients = False
+        continue
+
+    if find_fresh_ingredients:
+        fresh_ingredients.append({
+            "from": int(l.split("-")[0]),
+            "to": int(l.split("-")[1])
+        })
+    else:
+        available_ingredients.append(int(l))
+
+# ic(fresh_ingredients)
+# ic(available_ingredients)
+
+def solve_part1():
+    tm_start = time.time()
+    result_part1 = 0
+
+    # ic("DEBUG: TODO solve_part1()")
+
+    for id in available_ingredients:
+        # ic(f"checking freshness of {id}")
+        for r in fresh_ingredients:
+            if r["from"] <= id <= r["to"]:
+                # ic(f"ingredient {id} is fresh")
+                result_part1 += 1
+                break
+            # ic(f"ingredient {id} is spoiled according to {r}")
+
+    tm_end = time.time()
+    print(f"DEBUG: solve_part1 Begin: {time.ctime(tm_start)}")
+    print(f"DEBUG: solve_part1 End:   {time.ctime(tm_end)}")
+    print(f"DEBUG: solve_part1 Delta: {tm_end-tm_start}")
+    print(f"INFO:  Day{CHALLENGE_DAY:02} solve_part1 result: {result_part1}")
+    return result_part1
+
+
+def solve_part2():
+    tm_start = time.time()
+    result_part2 = 0
+
+    ic("DEBUG: TODO solve_part2()")
+
+    tm_end = time.time()
+    print(f"DEBUG: solve_part2 Begin: {time.ctime(tm_start)}")
+    print(f"DEBUG: solve_part2 End:   {time.ctime(tm_end)}")
+    print(f"DEBUG: solve_part2 Delta: {tm_end-tm_start}")
+    print(f"INFO:  Day{CHALLENGE_DAY:02} solve_part2 result: {result_part2}")
+    return result_part2
+
+
+if __name__ == "__main__":
+    solve_part1()
+    # check_valid_id_part2("1010")
+    # check_valid_id_part2("1011")
+    solve_part2()
+    pass
+
+# EOF
