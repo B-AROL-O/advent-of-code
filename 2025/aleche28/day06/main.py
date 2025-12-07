7d662068bcb894d691492e5dd48c7136e7fc9852
Alessio Chessa <ale.chessa28@gmail.com>
2025-12-07 12:40:18 +0100

    feat: aleche28 day 06 part 2 solution

--- 2025/aleche28/day06/main.py
+++ 2025/aleche28/day06/main.py
@@ -1,3 +1,5 @@
+import math
+
 
 def read_file_lines(filename):
     with open(filename, 'r') as f:
@@ -6,7 +8,7 @@ def read_file_lines(filename):
         return [[y for y in x if y.strip()] for x in split_lines]
 
 
-def main():
+def part1():
     grid = read_file_lines("input.txt")
     # print(grid)
     rows, cols = len(grid), len(grid[0])
@@ -23,6 +25,51 @@ def main():
         tot += col_tot
 
     print("Solution part 1: ", tot)
+
+
+def part2():
+    grid = []
+    with open('input.txt', 'r') as f:
+        grid = [[c for c in x.strip('\n')] for x in f]
+
+    ops = [x for x in grid[-1] if x.strip()]
+    grid = grid[:-1]
+    # print(grid, ops)
+    rows, cols = len(grid), len(grid[0])
+    operands = []
+    tot = 0
+    cur_op_idx = 0
+    for c in range(cols):
+        num = 0
+        for r in range(rows):
+            if grid[r][c] != ' ':
+                num = num*10 + int(grid[r][c])
+            elif num != 0:
+                break
+
+        # print(num)
+        if num == 0:
+            # empty column
+            if ops[cur_op_idx] == '+':
+                tot += sum(operands)
+            else:
+                tot += math.prod(operands)
+            # sum to sum
+            operands = []
+            cur_op_idx += 1
+        else:
+            operands.append(num)
+    if ops[cur_op_idx] == '+':
+        tot += sum(operands)
+    else:
+        tot += math.prod(operands)
+
+    print("Solution part 2: ", tot)
+
+
+def main():
+    part1()
+    part2()
 
 
 if __name__ == "__main__":
